38045015ad2184f3a2bd7cb5e6e94c5cdb1a650a
Luna <105922175+ImLunaHey@users.noreply.github.com>
2022-07-26 09:13:21 +0930

    fix: Router.use missing types

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,7 @@ export default SessionEngine;
 
 import Session from 'hyper-express-session/types/components/Session';
 import { Router as _Router } from 'hyper-express/types/components/router/Router';
+import { MiddlewareHandler } from 'hyper-express/types/components/middleware/MiddlewareHandler';
 
 declare module 'hyper-express' {
     interface Request {
@@ -12,5 +13,11 @@ declare module 'hyper-express' {
     class Router extends _Router {
         use(sessionMiddleware: SessionEngine): void;
         use(pattern: string, sessionMiddleware: SessionEngine): void;
+        use(router: Router): void;
+        use(...routers: Router[]): void;
+        use(...middlewares: MiddlewareHandler[]): void;
+        use(pattern: string, router: Router): void;
+        use(pattern: string, ...routers: Router[]): void;
+        use(pattern: string, ...middlewares: MiddlewareHandler[]): void;
     }
 }
