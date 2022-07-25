ae78b154124db19d151384b26032af9fc584be39
Luna <105922175+ImLunaHey@users.noreply.github.com>
2022-07-26 08:44:03 +0930

    fix: interface not extending Router class

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,14 +2,15 @@ import SessionEngine from "./components/SessionEngine";
 export default SessionEngine;
 
 import Session from 'hyper-express-session/types/components/Session';
+import { Router as _Router } from 'hyper-express/types/components/router/Router';
 
 declare module 'hyper-express' {
     interface Request {
         session: Session
     }
 
-    interface Router {
-        use(sessionMiddleware: Function): void;
-        use(pattern: string, sessionMiddleware: Function): void;
+    class Router extends _Router {
+        use(sessionMiddleware: SessionEngine): void;
+        use(pattern: string, sessionMiddleware: SessionEngine): void;
     }
 }
